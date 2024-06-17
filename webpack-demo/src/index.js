e24ad12d07cf5978148e341afc7e58bb0339a579
Marco Dal Boni <marco.dalboni@expandigroup.com>
2024-06-17 10:03:22 +0200

    .

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,9 +1,16 @@
 import myName from './myName';
+import printMe from './print';
 
 function component() {
     const element = document.createElement('div');
+    const button = document.createElement('button');    
 
     element.innerHTML = myName('Cody');
+
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
+  
+    element.appendChild(btn);
 
     return element;
 }
